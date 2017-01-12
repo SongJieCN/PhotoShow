b1f407f22bff99ebf222af248173ecabe33af86b
SONG.JIE <songjie1984@gmail.com>
2017-01-12 12:15:07 +0800

    Add Scrumb

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/scrumb', function(req, res, next) {
+    res.render('scrumb', { title: 'Express' });
+});
+
 module.exports = router;
