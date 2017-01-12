a407b2576547f092e0fa8f93d7c447091665fd53
song.jie <song.jie@cesgroup.com.cn>
2017-01-12 23:21:23 +0800

    Add mall

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,9 @@
-$(".card.first").on('swipeleft', function(){
-    $(".card.second").removeClass("moveTop");
-    $(".card.first").removeClass("moveBot");
-    $(".card.first").addClass("moveTop");
-    $(".card.second").addClass("moveBot");
-})
-
-$(".card.second").on('swipeleft', function(){
-    $(".card.first").removeClass("moveTop");
-    $(".card.second").removeClass("moveBot");
-    $(".card.second").addClass("moveTop");
-    $(".card.first").addClass("moveBot");
-})
+		$(function(){
+			$().timelinr({
+				orientation: 	'vertical',
+				issuesSpeed: 	300,
+				datesSpeed: 	100,
+				arrowKeys: 		'true',
+				startAt:		3
+			})
+		});
